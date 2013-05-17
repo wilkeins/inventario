fbf2b88274f409c049b4ae790c0221c28a12d1c6
wilkeins valera <wvalera26@gmail.com>
2013-05-17 17:26:58 -0430

    cambios

--- vista/index/index.php
+++ vista/index/index.php
@@ -1,4 +1,4 @@
-<div class="block_head">
+<!-- <div class="block_head">
                                     <div class="bheadl"></div>
                                     <div class="bheadr"></div>
                                     <h2>Bienvenido</h2>
@@ -26,5 +26,45 @@
         </div>
 
 </div>
-                                    </div>		<!-- .block_content ends -->
+                                    </div>	 -->	<!-- .block_content ends -->
 
+
+<div class="container">
+    <div class="bs-docs-example row">
+        <div class="descriptionForm">BIENVENIDO</div>
+        <a class="btn btn-large span3" style="margin-bottom:10px;" title="Usuarios" href="<?php echo URL; ?>usuario" >
+            <i class="icon-group icon-4x"></i>
+            <h3>Usuarios</h3>
+        </a>
+
+        <a class="btn btn-large span3" title="Usuarios" style="margin-bottom:10px;" href="<?php echo URL; ?>categoria" data-content="">
+            <i class="icon-list-ol icon-4x"></i>
+            <h3>Categorias</h3>
+        </a>
+
+        <a class="btn btn-large span3" title="Usuarios" style="margin-bottom:10px;" href="<?php echo URL; ?>muestra" data-content="">
+            <i class="icon-puzzle-piece icon-4x"></i>
+            <h3>Modelo</h3>
+        </a>
+
+        <a class="btn btn-large span3" title="Usuarios" style="margin-bottom:10px;" href="<?php echo URL; ?>cliente" data-content="">
+            <i class="icon-briefcase icon-4x"></i>
+            <h3>Clientes</h3>
+        </a>
+
+        <a class="btn btn-large span3" title="Usuarios" style="margin-bottom:10px;" href="<?php echo URL; ?>proveedor" data-content="">
+            <i class="icon-truck icon-4x"></i>
+            <h3>Proveedores</h3>
+        </a>
+
+        <a class="btn btn-large span3" title="Usuarios" style="margin-bottom:10px;" href="<?php echo URL; ?>producto" data-content="">
+            <i class="icon-gift icon-4x"></i>
+            <h3>Productos</h3>
+        </a>
+
+        <a class="btn btn-large span3" title="Usuarios" style="margin-bottom:10px;" href="<?php echo URL; ?>factura" data-content="">
+            <i class="icon-table icon-4x"></i>
+            <h3>Facturas</h3>
+        </a>
+    </div>
+</div>
